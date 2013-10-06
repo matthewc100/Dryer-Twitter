3e93ea83f607dd52926031b6e8307ffd7f12b813
matthewc100 <coblem@usxxcoblemm3.corp.emc.com>
2013-10-06 11:04:50 -0700

    Arduino-Twitter
    Various project items to tweet via WiFly module

---
+++ WiFly_Test/Credentials.h
@@ -0,0 +1,9 @@
+#ifndef __CREDENTIALS_H__
+#define __CREDENTIALS_H__
+
+// Wifi parameters
+char passphrase[] = "226z5hyi";
+char ssid[] = "LittleDeer";
+char ntp_server[] = "nist1-la.ustiming.org"; // or your favorite ntp server.
+
+#endif
